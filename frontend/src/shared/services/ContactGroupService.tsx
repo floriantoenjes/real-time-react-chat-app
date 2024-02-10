7f537e746d7b3b8b1c390b1eb0bc1bc00d654d30
Florian Toenjes <skatepainter@yahoo.de>
2024-02-10 17:26:52 +0100

    Add ContactGroupService in frontend
    This commit introduces a new ContactGroupService in frontend. This service includes methods for fetching, adding and deleting contact groups, using a client initialized with contactGroupContract. It interacts with the backend through API calls.

---
+++ frontend/src/shared/services/ContactGroupService.tsx
@@ -0,0 +1,45 @@
+import { initClient } from "@ts-rest/core";
+import { BACKEND_URL } from "../../environment";
+import {
+    ContactGroup,
+    contactGroupContract,
+} from "real-time-chat-backend/dist/shared/contact-group.contract";
+
+export class ContactService {
+    private client;
+
+    constructor() {
+        this.client = initClient(contactGroupContract, {
+            baseUrl: BACKEND_URL,
+            baseHeaders: {},
+        });
+    }
+
+    async getContactGroups(userId: string): Promise<ContactGroup[]> {
+        const messages = await this.client.getContactGroups({
+            body: { userId },
+        });
+
+        if (messages.status === 200) {
+            return messages.body;
+        }
+
+        return [];
+    }
+
+    async addContactGroup(
+        userId: string,
+        groupName: string,
+        memberIds: string[],
+    ) {
+        return await this.client.addContactGroup({
+            body: { userId, groupName, memberIds },
+        });
+    }
+
+    async deleteContactGroup(userId: string, contactGroupId: string) {
+        return await this.client.removeContactGroup({
+            body: { userId, contactGroupId },
+        });
+    }
+}
