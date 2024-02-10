2a3c2c88cf2ce58aaa12b65c3996dc0015dcc9f4
Florian Toenjes <skatepainter@yahoo.de>
2024-02-10 12:03:29 +0100

    started implementing chat group creation

--- frontend/src/dashboard/sidebar/top-section/TopSection.tsx
+++ frontend/src/dashboard/sidebar/top-section/TopSection.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-import { Button, IconButton, Menu, MenuItem } from "@mui/material";
+import { Button, Drawer, IconButton, Menu, MenuItem } from "@mui/material";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import "./TopSection.css";
 
 export function TopSection() {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -13,6 +15,28 @@ export function TopSection() {
         setAnchorEl(null);
     };
 
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer =
+        (anchor: string, open: boolean) =>
+        (event: React.KeyboardEvent | React.MouseEvent) => {
+            handleClose();
+            if (
+                event.type === "keydown" &&
+                ((event as React.KeyboardEvent).key === "Tab" ||
+                    (event as React.KeyboardEvent).key === "Shift")
+            ) {
+                return;
+            }
+
+            setState({ ...state, [anchor]: open });
+        };
+
     return (
         <div className={"flex items-center"}>
             <Link to="/">
@@ -23,9 +47,27 @@ export function TopSection() {
                     <ChevronDownIcon className={"w-8"} />
                 </IconButton>
                 <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                    <MenuItem onClick={handleClose}>Create a group</MenuItem>
+                    <MenuItem onClick={toggleDrawer("left", true)}>
+                        New group
+                    </MenuItem>
                 </Menu>
             </div>
+            <Drawer
+                anchor={"left"}
+                open={state["left"]}
+                onClose={toggleDrawer("left", false)}
+            >
+                <div className={"drawer"}>
+                    <div
+                        className={
+                            "drawer-head flex justify-center items-center"
+                        }
+                    >
+                        <ArrowLeftIcon className={"w-8"} />
+                        <h4 className={"ml-3"}>Gruppenmitglieder hinzufügen</h4>
+                    </div>
+                </div>
+            </Drawer>
         </div>
     );
 }
