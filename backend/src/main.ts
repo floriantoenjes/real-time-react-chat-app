797f3c2ae79647b97230d22d2aeea9f9db0fc855
Florian Toenjes <skatepainter@yahoo.de>
2024-02-17 23:19:00 +0100

    added db connection log statement to see what is going on

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(
+    `Connecting to uri: ${process.env.uri} with username: ${process.env.user} and dbName: ${process.env.dbName}`,
+  );
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(4200);
