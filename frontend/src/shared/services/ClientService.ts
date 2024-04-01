80cb8ffd201c174ef703f1388e9305cf54aac9bc
Florian Toenjes <skatepainter@yahoo.de>
2024-04-01 17:11:07 -0300

    refactor: improved typing of ClientService.ts

--- frontend/src/shared/services/ClientService.ts
+++ frontend/src/shared/services/ClientService.ts
@@ -1,10 +1,34 @@
-import { initClient } from "@ts-rest/core";
+import {
+    AppRoute,
+    AppRouteFunction,
+    AppRouter,
+    initClient,
+    InitClientArgs,
+} from "@ts-rest/core";
 import { BACKEND_URL, LOCAL_STORAGE_AUTH_KEY } from "../../environment";
+
+type RecursiveProxyObj<T, TClientArgs extends InitClientArgs> = {
+    [TKey in keyof T]: T[TKey] extends AppRoute
+        ? AppRouteFunction<T[TKey], TClientArgs>
+        : T[TKey] extends AppRouter
+          ? RecursiveProxyObj<T[TKey], TClientArgs>
+          : never;
+};
 
 export class ClientService {
     private contractClientMap: Map<any, any> = new Map();
 
-    getClient(contract: any) {
+    getClient<T extends AppRouter>(
+        contract: T,
+    ): RecursiveProxyObj<
+        T,
+        {
+            baseUrl: string;
+            baseHeaders: {
+                Authorization: string;
+            };
+        }
+    > {
         if (this.contractClientMap.get(contract)) {
             return this.contractClientMap.get(contract);
         }
@@ -21,7 +45,11 @@ export class ClientService {
             }),
         );
 
-        return this.contractClientMap.get(contract);
+        const clientInMap = this.contractClientMap.get(contract);
+        if (!clientInMap) {
+            throw new Error("TS-Rest client not found in Map!");
+        }
+        return clientInMap;
     }
 
     clearBearerTokenAndClients() {
