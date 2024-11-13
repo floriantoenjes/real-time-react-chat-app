b6f23094b60c4540d907aaf33ab27d0825ec321c
Florian Tönjes <skatepainter@yahoo.de>
2024-11-13 21:43:55 +0100

    feat: v1.4.6; re-added environments file for frontend

---
+++ frontend/src/environment.ts
@@ -0,0 +1,10 @@
+export const BACKEND_URL = import.meta.env.PROD ? "" : "http://localhost:4200";
+
+export const PEERJS_SERVER_HOST = import.meta.env.VITE_PEERJS_SERVER_HOST;
+export const PEERJS_SERVER_PORT = import.meta.env.VITE_PEERJS_SERVER_PORT;
+
+export const TURN_SERVER_URL = import.meta.env.VITE_TURN_SERVER_URL;
+export const TURN_SERVER_USERNAME = import.meta.env.VITE_TURN_SERVER_USERNAME;
+export const TURN_SERVER_PASSWORD = import.meta.env.VITE_TURN_SERVER_PASSWORD;
+
+export const LOCAL_STORAGE_AUTH_KEY = "signedIn";
