8f3155627a072058ec7b8b74a6b85268e9c5925e
floriantoenjes <skatepainter@yahoo.de>
2026-01-22 14:20:19 +0100

    chore: upgrade Vite and remove obsolete vite- plugin-require

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
-import vitePluginRequire from "vite-plugin-require";
 import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vitePluginRequire.default(), commonjs(), react()],
+    plugins: [commonjs(), react()],
     base: "/frontend/",
     // build: {
     //     sourcemap: "inline", // TODO: Comment out after debugging
