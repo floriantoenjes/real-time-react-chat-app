d94f4891ca8fb6668bc99c81e2c27deb59f0e122
floriantoenjes <skatepainter@yahoo.de>
2026-05-28 18:03:18 +0200

    feature: add ignore feature frontend service

---
+++ frontend/src/shared/services/IgnoredUserService.ts
@@ -0,0 +1,30 @@
+import { ClientService } from "./ClientService";
+import { ignoredUserContract } from "@t/ignored-user.contract";
+
+export class IgnoredUserService {
+    constructor(private readonly clientService: ClientService) {}
+
+    async ignoreUser(ignoredUserId: string) {
+        return this.clientService
+            .getClient(ignoredUserContract)
+            .ignoreUser({ body: { ignoredUserId } });
+    }
+
+    async unignoreUser(ignoredUserId: string) {
+        return this.clientService
+            .getClient(ignoredUserContract)
+            .unignoreUser({ body: { ignoredUserId } });
+    }
+
+    async getIgnoredUsers(page?: number, limit?: number) {
+        return this.clientService
+            .getClient(ignoredUserContract)
+            .getIgnoredUsers({ query: { page, limit } });
+    }
+
+    async ignoreFromContactRequest(contactRequestId: string) {
+        return this.clientService
+            .getClient(ignoredUserContract)
+            .ignoreFromContactRequest({ body: { contactRequestId } });
+    }
+}
