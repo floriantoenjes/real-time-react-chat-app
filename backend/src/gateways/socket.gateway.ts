323655ba463eacdb180b5b4bcc7d923650b6fd53
floriantoenjes <skatepainter@yahoo.de>
2026-06-06 09:43:17 +0200

    refactor: formatting

--- backend/src/gateways/socket.gateway.ts
+++ backend/src/gateways/socket.gateway.ts
@@ -63,9 +63,7 @@ export class RealTimeChatGateway
      */
     onModuleInit(): void {
         this.processMessageEvents();
-
         this.processContactEvents();
-
         this.processIgnoreEvents();
     }
 
